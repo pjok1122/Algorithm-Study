afccada75a1b1ab21537b34ac26e629db30be196
leehani <rorean@naver.com>
2019-12-24 13:12:43 +0900

    프로그래머스 미해결 문제

---
+++ programmers/정수제곱근판별.py
@@ -0,0 +1,12 @@
+def solution(n):
+    if n == 1:
+        return 1
+    for i in range(n//2):
+        if i*i == n:
+            return (i+1)*(i+1)
+    return -1
+print(solution(3))
+print(solution(121))
+print(solution(144))
+print(solution(1))
+print(solution(2))
